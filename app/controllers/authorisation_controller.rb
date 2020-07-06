d6fa7fa4381e3777000b89329de56015ab0716de
rony0203 <67779519+rony0203@users.noreply.github.com>
2020-07-06 14:16:03 +0300

    v6

--- app/controllers/authorisation_controller.rb
+++ app/controllers/authorisation_controller.rb
@@ -2,7 +2,7 @@ class AuthorisationController < ApplicationController
   def login
     @user = User.find_by_email(params[:email])
   if @user && @user.authenticate(params[:password])
-    @token = Token.create(user_id: @user)
+  @token = Token.create(user_id: @user)
       render json: {user: @user, token: @token}
 
     else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+before_action :authenticate, only: :show
 class UsersController < ApplicationController
    def new
       @user = User.new #-> invokes a new user object
@@ -12,6 +13,12 @@ class UsersController < ApplicationController
 
    def show
       @user = User.find params[:id] #-> stateless means you have to rebuild the object again
+      current_user = Token.find_by(token: token)&.user
+if current_user.present?
+  render json: @user
+else
+  render json: { error: 'Not Authorized' }, status: :unauthorized
+end
    end
    def user_params
    params.require(:user).permit(:name, :email, :password)
