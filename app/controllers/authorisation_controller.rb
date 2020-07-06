ab632b7e88f352db349412e4c73e01c7fa26b245
rony0203 <67779519+rony0203@users.noreply.github.com>
2020-07-06 11:16:29 +0300

    v4

--- app/controllers/authorisation_controller.rb
+++ app/controllers/authorisation_controller.rb
@@ -1,6 +1,7 @@
 class AuthorisationController < ApplicationController
   def login
     @user = User.find_by_email(params[:email])
+    binding.pry
     if @user && @user.authenticate(params[:password])
       render json: @user
 
