e6112af9623757e22750b460b4b7facc3e69ae8e
rony0203 <67779519+rony0203@users.noreply.github.com>
2020-07-06 14:19:05 +0300

    v6

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'users/new'
   post 'users/create'
   post 'authorisation/login'
+  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
